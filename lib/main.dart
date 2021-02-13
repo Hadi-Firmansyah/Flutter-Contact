4fea068458e0b2c70106a547538298bfabbb01e4
Hadi Firmansyah <hadifirmansyah.id@gmail.com>
2021-02-13 10:21:42 +0700

    test

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //judul
+      title: 'Tambahkan Daftar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Home(),
+    );
+  }
+}
